f76895f248e73296b09f48e9dc615c1be42e254c
Kaoru <SHIINA_KAORU@outlook.com>
2021-08-11 02:00:14 +0800

    add thread pool shutdown function and config files

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <threadPool.h>
+
+volatile int i = 0;
+
+static void do_something() { i++; }
+
+int main() {
+  ThreadPool pool{8};
+  for (int j = 0; j < 50000; j++) {
+    pool.execute(do_something);
+  }
+  pool.shutdown();
+  return 0;
+}
